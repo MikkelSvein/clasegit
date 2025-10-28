d4940754ce6e99a26da6567b47e0d39379358bb0
Mikkel Svein <109176630+MikkelSvein@users.noreply.github.com>
2025-10-27 19:37:39 -0500

    configuracion index

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const comicsRouter = require('./routes/comics');
 
 const app = express();
 const PORT = 3000;
-app.use
+app.use(express.json());
+app.use("/comics", comicsRouter);
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
--- routes/comics.js
+++ routes/comics.js
@@ -1,4 +1,4 @@
-const router = require('express').Router();
+const comicsRouter = require('express').Router();
 const comicsController = require('../controllers/comics');
-router.get('/comics', comicsController.getComics);
-module.exports = router;
+comicsRouter.get('/todo', comicsController.getComics);
+module.exports = comicsRouter
