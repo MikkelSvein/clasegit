dcf31f2fe8d0b3068387e0ef9ebacb2d6b60fee5
camilo <juan.londono2@estudiantesunibague.edu.co>
2025-10-29 18:57:35 -0500

    Agregado end point type

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const comicsRouter = require('./routes/comics');
 
 const app = express();
+
 process.loadEnvFile();
+
 const PORT = 3000;
+
 app.use(express.json());
+
 app.use("/comics", comicsRouter);
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
