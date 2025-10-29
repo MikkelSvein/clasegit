2606be97f16db18ee584dc9dae5ed62026625e1a
Mikkel Svein <109176630+MikkelSvein@users.noreply.github.com>
2025-10-29 18:39:24 -0500

    creacion .env y pokemon api

--- controllers/comics.js
+++ controllers/comics.js
@@ -1,7 +1,11 @@
 const comicsController = {};
+const axios = require('axios');
 
 comicsController.getComics = (req, res) => {
-    res.json({ message: 'List of comics' });
+    const POKEMON_API = process.env.POKEMON_API;
+
+    axios.get(POKEMON_API + '/pokemon')
+    .then(resp => { res.json(resp.data);})
 }
 
 module.exports = comicsController;
