445597ed1b0199f5152c671cb692ad7ecfcddc4f
Mikkel Svein <109176630+MikkelSvein@users.noreply.github.com>
2025-10-29 19:06:48 -0500

    creacion controller species

---
+++ controllers/species.js
@@ -0,0 +1,11 @@
+const comicsController = {};
+const axios = require('axios');
+
+comicsController.getComics = (req, res) => {
+    const POKEMON_API = process.env.POKEMON_API;
+
+    axios.get(POKEMON_API + '/pokemon')
+    .then(resp => {res.json(resp.data);})
+}
+
+module.exports = comicsController;
